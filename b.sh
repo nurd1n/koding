6f682b31f45d83349efee41bfbee13c8ab0439e9
Nurdianto <nurdiantopntr@gmail.com>
2015-12-24 18:42:19 +0700

    Create b.sh

---
+++ b.sh
@@ -0,0 +1,13 @@
+sudo apt-get install tightvncserver
+vncserver :1 -geometry 1200x600 -depth 16 -pixelformat rgb565
+sudo echo "lxterminal &
+/usr/bin/lxsession -s LXDE &" >> ~/.vnc/xstartup
+sudo apt-get install firefox
+#install bleachbit as ccleaner
+sudo apt-get install bleachbit
+#install gedit
+sudo apt-get install gedit
+#install terminator
+sudo apt-get install terminator
+#install filezilla
+sudo apt-get install filezilla
